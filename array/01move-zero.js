0be4e309a06dad1ee57495d439d8de755de90049
wangkaiwd <1162486176@qq.com>
2020-11-09 17:51:11 +0800

    feat: add animation link

--- array/01move-zero.js
+++ array/01move-zero.js
@@ -10,6 +10,8 @@
 //   You must do this in-place without making a copy of the array.
 //   Minimize the total number of operations.
 
+// diagram: https://leetcode-cn.com/problems/move-zeroes/solution/dong-hua-yan-shi-283yi-dong-ling-by-wang_ni_ma/
+
 // 暴力解法： 将所有元素往前移动一位，将0放到数组的最后一位。 问题： 会导致数组的遍历出现问题,并且移动之后的0还会继续参与遍历
 const nums = [0, 1, 2, 0, 3, 12];
 
