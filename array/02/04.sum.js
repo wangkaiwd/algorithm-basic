58a94da5b382ada8e6a0ef9cfd71358fdcaa19b3
wangkaiwd <1162486176@qq.com>
2020-11-16 18:04:16 +0800

    feat: preliminary accomplish three sum

--- array/02/04.sum.js
+++ array/02/04.sum.js
@@ -12,3 +12,27 @@
 // Example 3:
 // Input: nums = [0]
 // Output: []
+
+// 暴力解法：
+//  首先排序，从小到大进行排列，并且将相同元素放到一块儿，然后通过三重循环遍历所有元素的和
+//  如果当前的数值和前一个数值相同，说明会和前一个找到相同的结果，所以需要跳过
+//  如果nums[i] > 0, 那么三个数字之和永远不可能是0
+const nums = [-1, 0, 1, 2, -1, -4];
+const threeSum1 = (nums) => {
+  const indices = [];
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (nums[i] > 0) {return indices;}
+    if (i > 0 && (nums[i] === nums[i - 1])) {continue;}
+    for (let j = i + 1; j < nums.length - 1; j++) {
+      if (j > (i + 1) && (nums[j] === nums[j - 1])) {continue;}
+      for (let k = j + 1; k < nums.length; k++) {
+        if (k > (j + 1) && (nums[k] === nums[k - 1])) {continue;}
+        if (nums[i] + nums[j] + nums[k] === 0) {
+          indices.push([i, j, k]);
+        }
+      }
+    }
+  }
+  return indices;
+};
+console.log('indices', threeSum1(nums));
