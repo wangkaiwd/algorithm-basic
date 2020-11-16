a71d302cee182b6b636c59207b25d47d42b4ee5c
wangkaiwd <1162486176@qq.com>
2020-11-16 10:50:28 +0800

    feat: recursive implement reverse linked list

--- linked-list/01.reverse-linked-list.js
+++ linked-list/01.reverse-linked-list.js
@@ -24,8 +24,19 @@
 // console.log(reverseList(head));
 //
 
+// https://leetcode-cn.com/problems/fan-zhuan-lian-biao-lcof/solution/fan-zhuan-lian-biao-yi-dong-de-shuang-zhi-zhen-jia/
 // 递归，由于创建了n个函数，所以空间复杂度：O(n)
 // const head = [1, 2, 3, 4, 5];
-const reverseList = (head) => {
-  // 递归进行处理
-};
+// // 1->2->3->4->5->NULL
+// const reverseList1 = (head) => {
+//   // 递归进行处理
+//   if (head == null || head.next == null) {
+//     return head;
+//   }
+//   const result = reverseList1(head.next);
+//   head.next.next = head;
+//   head.next = null;
+//   return result;
+// };
+
+
