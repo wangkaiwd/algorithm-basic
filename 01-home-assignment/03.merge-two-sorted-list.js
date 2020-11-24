d1cc47d3355bf57c11da857fe19594c227c8daf4
wangkaiwd <1162486176@qq.com>
2020-11-24 16:20:58 +0800

    feat: accomplish merge two sorted list

--- 01-home-assignment/03.merge-two-sorted-list.js
+++ 01-home-assignment/03.merge-two-sorted-list.js
@@ -11,6 +11,29 @@
 // Input: l1 = [], l2 = [0]
 // Output: [0]
 
-const mergeTwoLists = (l1, l2) => {
+// 输入：1->2->4, 1->3->4
+// 输出：1->1->2->3->4->4
+// const l1 = { val: 1, next: { val: 2, next: { val: 4, next: null } } };
+const l1 = { val: 1, next: { val: 2, next: null } };
+// const l2 = { val: 1, next: { val: 3, next: { val: 4, next: null } } };
+const l2 = { val: 1, next: { val: 3, next: null } };
 
+const mergeTwoLists = (l1, l2) => {
+  // 终止条件
+  if (l1 == null) {
+    return l2;
+  }
+  if (l2 == null) {
+    return l1;
+  }
+  if (l1.val < l2.val) {
+    // 2. {val:1, next: ?}
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  } else {
+    // 1. {val: 1 , next: ?}
+    l2.next = mergeTwoLists(l1, l2.next);
+    return l2;
+  }
 };
+console.log(mergeTwoLists(l1, l2));
