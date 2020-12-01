1ec401c2a784eceac70d2d6ad3e4c9afe15bf16f
wangkaiwd <1162486176@qq.com>
2020-12-02 00:52:49 +0800

    feat: accomplish two sum by hash table

--- 01-home-assignment/05.two-sum.js
+++ 01-home-assignment/05.two-sum.js
@@ -15,6 +15,38 @@
 // Input: nums = [3,3], target = 6
 // Output: [0,1]
 
-const twoSum = (nums, target) => {
-
+const twoSum1 = (nums, target) => {
+  let result = undefined;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        result = [i, j];
+      }
+    }
+  }
+  return result;
 };
+
+const nums = [2, 7, 11, 15];
+console.log(twoSum1(nums, 9));
+
+// 使用一个映射表来存储遍历过的元素，key为元素，val为其索引
+// 遍历时判断 target-nums[i] 是否在数组之前的元素中存在，如果存在，直接返回
+// 使用空间来换时间
+const twoSum2 = (nums, target) => {
+  let result = undefined;
+  const map = {};
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (String(complement) in map) {
+      result = [map[complement], i];
+      break;
+    }
+    map[nums[i]] = i;
+  }
+
+  return result;
+};
+
+console.log(twoSum2(nums, 9));
+
