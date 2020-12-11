6c43286112203f63d7931e451a7a0cf7ec12f706
wangkaiwd <1162486176@qq.com>
2020-12-12 00:13:07 +0800

    feat: think random reference pointer

--- source/quickSort/demo01.js
+++ source/quickSort/demo01.js
@@ -1,5 +1,5 @@
-// O(n平方)，性能感觉不是很好
-function partition (arr) {
+// 原地排序：O(n平方)，性能感觉不是很好
+function partition1 (arr) {
   let index = 0;
   const delimiter = arr[0];
   for (let i = 1; i < arr.length; i++) {
@@ -16,4 +16,10 @@ function partition (arr) {
 }
 
 const arr = [4, 3, 2, 5, 8, 7, 1];
-console.log(partition(arr), arr);
+console.log(partition1(arr), arr);
+
+// 非原地实现：声明left,right俩个数组，然后将小于delimiter的值放到left数组中，大于delimiter的值放到right数组中，最后将三部分拼接起来
+
+// 原地实现2：
+//  l 为参考元素的索引
+//  将原数组分为俩个区间：[l+1,j] < v, [j+1,i] > v
