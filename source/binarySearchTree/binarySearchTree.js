a33c968e70ac4c8c95ad246fae23f2ce9e173760
wangkaiwd <1162486176@qq.com>
2020-12-16 16:47:38 +0800

    feat: add method for binary search tree but not concrete implement

--- source/binarySearchTree/binarySearchTree.js
+++ source/binarySearchTree/binarySearchTree.js
@@ -1,3 +1,49 @@
-class BinarySearchTree {
-
+class Node {
+  constructor (element, left = null, right = null) {
+    this.element = element;
+    this.left = left;
+    this.right = right;
+  }
 }
+
+class BinarySearchTree {
+  constructor (root = null) {
+    this.root = null;
+    this.left = root.left || null;
+    this.right = root.right || null;
+    this.size = 0;
+  }
+
+  getSize () {
+    return this.size;
+  }
+
+  isEmpty () {
+    return this.size === 0;
+  }
+
+  contains (e) {
+
+  }
+
+  add (e, node = this.root) {
+
+  }
+
+  // 前序遍历
+  preOrder () {
+
+  }
+
+  // 中序遍历
+  inOrder () {
+
+  }
+
+  // 后续遍历
+  postOrder () {
+
+  }
+}
+
+module.exports = BinarySearchTree;
