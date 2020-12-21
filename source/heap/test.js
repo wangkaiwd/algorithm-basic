015d75cf06d317e6da6b9dc6a7ec85d894f1bcfd
wangkaiwd <1162486176@qq.com>
2020-12-22 01:05:30 +0800

    feat: implement heap sort

--- source/heap/test.js
+++ source/heap/test.js
@@ -1,12 +1,18 @@
 const MaxHeap = require('./maxHeap');
-const { genRandomArray } = require('../shared/sortHelper');
+const { genRandomArray, isSorted } = require('../shared/sortHelper');
 
 function test () {
   const maxHeap = new MaxHeap();
-  const arr = genRandomArray(20);
-  for (let i = 0; i < 8; i++) {
+  const n = 200;
+  const arr = genRandomArray(n);
+  for (let i = 0; i < n; i++) {
     maxHeap.add(arr[i]);
   }
+  const result = [];
+  for (let i = 0; i < n; i++) {
+    maxHeap.extractMax();
+  }
+  console.log(isSorted(result));
 }
 
 test();
