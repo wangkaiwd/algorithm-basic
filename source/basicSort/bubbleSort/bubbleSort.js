6f0d95054e4f90f2aa742ea91014e8635da50112
wangkaiwd <1162486176@qq.com>
2020-12-24 01:03:00 +0800

    feat: skip some has sorted turn

--- source/basicSort/bubbleSort/bubbleSort.js
+++ source/basicSort/bubbleSort/bubbleSort.js
@@ -3,7 +3,7 @@ const { swap } = require('../../shared/utils');
 const bubbleSort = (arr) => {
   // 相邻元素进行对比，每轮循环都会确定一个最大值，并可以让元素变得更有序
   const len = arr.length;
-  for (let i = 0; i < len; i++) {
+  for (let i = 0; i < len - 1; i++) {
     // 每轮移动都能确定末尾的一个元素的位置，所以它不用再参与遍历
     for (let j = 0; j < len - i - 1; j++) {
       // 每一个元素都和它的后一个元素比较，将相邻元素中较大的后移
@@ -17,7 +17,7 @@ const bubbleSort = (arr) => {
 
 // 当处理的是排序好的内容时，第一轮循环不会进行元素的交换操作
 const bubbleSort2 = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length - 1; i++) {
     let isSwapped = false;
     for (let j = 0; j < arr.length - i - 1; j++) {
       if (arr[j + 1] < arr[j]) {
@@ -31,6 +31,29 @@ const bubbleSort2 = (arr) => {
   }
 };
 
-const arr = genRandomArray(100);
+// 当最后的几次都没有进行交换时，说明最后的几个元素都是已经排序好的元素
+const bubbleSort3 = (arr) => {
+  for (let i = 0; i < arr.length - 1;) {
+    let lastSwapIndex = 0;
+    // [j+1, arr.length-1] 都是排序好的
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+        lastSwapIndex = j;
+      }
+    }
+    // 得到最后交换元素的index
+    // 直接跳过arr.length - lastSwapIndex轮的比较
+    i = arr.length - lastSwapIndex - 1;
+  }
+};
+// const arr = genRandomArray(8);
+const arr = [2, 5, 1, 1, 7, 5, 0, 0];
+const arr2 = [...arr];
+const arr3 = [...arr];
 bubbleSort(arr);
+bubbleSort2(arr2);
+bubbleSort3(arr3);
 console.log(isSorted(arr));
+console.log(isSorted(arr2));
+console.log(isSorted(arr3));
