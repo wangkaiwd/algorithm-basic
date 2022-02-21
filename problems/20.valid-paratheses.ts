191d430b9e30e48e7efdffb2d7e0f1ec35e84a15
wangkaiwd <1162486176@qq.com>
2022-02-21 21:38:48 +0800

    feat: optimize algorithm

---
+++ problems/20.valid-paratheses.ts
@@ -0,0 +1,27 @@
+const isValidBrackets = (string: string) => {
+  const stack: string[] = [];
+  const map = {
+    '{': '}',
+    '(': ')',
+    '[': ']'
+  };
+  for (let i = 0; i < string.length; i++) {
+    const current = stack[stack.length - 1];
+    const element = string[i];
+    if ((map as any)[element]) { // left bracket enqueue
+      stack.push(element);
+    } else {
+      if ((map as any)[current] === element) { // closed bracket, dequeue
+        stack.pop();
+      }
+      // must invalid
+      // ([{ -> next only } may be valid
+      // ([{ ],) must invalid
+      return false;
+    }
+  }
+  return stack.length === 0;
+};
+
+console.log(isValidBrackets('()[]{}'));
+console.log(isValidBrackets('(]'));
