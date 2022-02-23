b16e9b0efd5cf8a759669b5f5acb8d5686c5fc1c
wangkaiwd <1162486176@qq.com>
2022-02-24 00:09:16 +0800

    feat: create types for reverse linked list

---
+++ problems/206.reverse-linked-list.ts
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+import { ListNode } from './types';
+
+// 1 -> 2 -> 3 -> 4 -> null
+// 4 -> 3 -> 2 -> 1 -> null
+
+function reverseList (head: ListNode | null): ListNode | null {
+
+  return head;
+}
--- problems/237.delete-node-in-a-linked-list.ts
+++ problems/237.delete-node-in-a-linked-list.ts
@@ -1,8 +1,3 @@
-interface ListNode {
-  val: number;
-  next: ListNode;
-}
-
 // class ListNode {
 //   val: number;
 //   next: ListNode | null;
@@ -15,6 +10,8 @@ interface ListNode {
 // 1 -> 2 -> 3 -> 4 -> null
 // node = 3
 // node = node.next
+import { ListNode } from './types';
+
 function deleteNode (node: ListNode): void {
   node.val = node.next.val;
   node.next = node.next.next;
