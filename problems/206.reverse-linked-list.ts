089c1840f7df29756e37b8d822e7b3bdaeb8b62b
wangkaiwd <1162486176@qq.com>
2022-02-26 23:33:32 +0800

    feat: reverse linked list, loop and recursive

--- problems/206.reverse-linked-list.ts
+++ problems/206.reverse-linked-list.ts
@@ -14,7 +14,28 @@ import { ListNode } from './types';
 // 1 -> 2 -> 3 -> 4 -> null
 // 4 -> 3 -> 2 -> 1 -> null
 
-function reverseList (head: ListNode | null): ListNode | null {
+// next = cur.next
+// cur.next = prev
+// prev = cur
+// cur = next
+// until cur = null
 
-  return head;
+type NodeOrNull = ListNode | null
+
+// prev, cur, next
+function reverse (prev: NodeOrNull, cur: NodeOrNull, next: NodeOrNull): NodeOrNull {
+  if (cur === null) {
+    return prev;
+  }
+  next = cur.next;
+  cur.next = prev;
+  prev = cur;
+  cur = next;
+  return reverse(prev, cur, next);
 }
+
+function reverseList (head: NodeOrNull): NodeOrNull {
+  return reverse(null, head, null);
+}
+
+
