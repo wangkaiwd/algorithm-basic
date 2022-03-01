e05f6dc2dbc6c388b39b0bf317848a5275d8bc4b
wangkaiwd <1162486176@qq.com>
2022-03-02 01:10:44 +0800

    feat: optimize function body

--- problems/349.intersection-of-two-arrays.ts
+++ problems/349.intersection-of-two-arrays.ts
@@ -1,16 +1,17 @@
 // O(n): Set, iterable protocol, iterator protocol, Set/Map is built-in iterables
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Iteration_protocols
 function intersection (nums1: number[], nums2: number[]): number[] {
-  const set1 = new Set(nums1);
-  const set2 = new Set(nums2);
-  const set3 = new Set<number>();
-
-  set1.forEach(item => {
-    if (set2.has(item)) {
-      set3.add(item);
-    }
-  });
-  return [...set3];
+  // const set1 = new Set(nums1);
+  // const set2 = new Set(nums2);
+  // const set3 = new Set<number>();
+  //
+  // set1.forEach(item => {
+  //   if (set2.has(item)) {
+  //     set3.add(item);
+  //   }
+  // });
+  // return [...set3];
+  return [...new Set(nums1)].filter((num) => new Set(nums2).has(num));
 }
 
 // first sort array: o(nlogn)
