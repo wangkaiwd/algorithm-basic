7c16597bc30b201c4d4e5128d1ebf9e46e06cf70
wangkaiwd <1162486176@qq.com>
2022-03-03 00:16:43 +0800

    chore: optimize by in operator

--- problems/1.two-sum.ts
+++ problems/1.two-sum.ts
@@ -4,10 +4,11 @@ function twoSum (nums: number[], target: number): any {
   for (let i = 0; i < nums.length; i++) {
     const num = nums[i];
     const diff = target - num;
-    if (map[diff] !== undefined) {
+    if (diff in map) {
       return [map[diff], i];
     } else {
       map[num] = i;
     }
   }
+  throw new Error('No two sum solution!');
 }
